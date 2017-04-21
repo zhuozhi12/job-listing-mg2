f239ad63d83e65fa5b026e008d3d4003608162a4
zhuozhi12 <feibou@126.com>
2017-04-21 16:54:42 +0800

    implement wage part

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -49,6 +49,7 @@ before_action :require_is_admin
   private
 
   def job_params
-    params.require(:job).permit(:title, :description)
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
   end
+  
 end
